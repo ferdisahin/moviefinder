ce15e9d7120ecd639d37e95ed0de1fb5e230c3a8
ferdisahin <ferdisahin@hotmail.fr>
2024-04-28 01:33:27 +0300

    updated

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,10 @@
-import baseURL from "@/utils/functions";
+const baseURL =
+	process.env.NODE_ENV === "production"
+		? "https://moviefinder-sooty.vercel.app/"
+		: "http://localhost:3000";
 
 export const getData = async (slug, limit = null, lang = "en-US", page = null) => {
-	const res = await fetch(`${baseURL()}/api/${slug}?lang=${lang}${page && `&page=${page}`} `)
+	const res = await fetch(`${baseURL}/api/${slug}?lang=${lang}${page && `&page=${page}`} `)
 
 	if(!res.ok){
 		throw new Error('Failed to fetch data')
@@ -15,7 +18,7 @@ export const getData = async (slug, limit = null, lang = "en-US", page = null) =
 }
 
 export const getMovie = async(id, lang = "en-US") => {
-	const res = await fetch(`${baseURL()}/api/get-movie?id=${id}&lang=${lang}`)
+	const res = await fetch(`${baseURL}/api/get-movie?id=${id}&lang=${lang}`)
 
 	if(!res.ok){
 		throw new Error('Failed to fetch movie')
@@ -25,7 +28,7 @@ export const getMovie = async(id, lang = "en-US") => {
 }
 
 export const getVideo = async(id, lang = "en-US") => {
-	const res = await fetch(`${baseURL()}/api/get-video?id=${id}&lang=${lang}`)
+	const res = await fetch(`${baseURL}/api/get-video?id=${id}&lang=${lang}`)
 
 	if(!res.ok){
 		throw new Error('Failed to fetch video')
@@ -35,7 +38,7 @@ export const getVideo = async(id, lang = "en-US") => {
 }
 
 export const getCredits = async(id, lang = "en-US") => {
-	const res = await fetch(`${baseURL()}/api/get-credits?id=${id}&lang=${lang}`)
+	const res = await fetch(`${baseURL}/api/get-credits?id=${id}&lang=${lang}`)
 
 	if(!res.ok){
 		throw new Error('Failed to fetch credits')
@@ -45,7 +48,7 @@ export const getCredits = async(id, lang = "en-US") => {
 }
 
 export const getRelated = async(id, lang = "en-US") => {
-	const res = await fetch(`/api/get-related?id=${id}&lang=${lang}`)
+	const res = await fetch(`${baseURL}/api/get-related?id=${id}&lang=${lang}`)
 
 	if(!res.ok){
 		throw new Error('Failed to fetch related movies')
@@ -55,7 +58,7 @@ export const getRelated = async(id, lang = "en-US") => {
 }
 
 export const getGenre = async(platform = 'movies', lang = "en-US") => {
-	const res = await fetch(`${baseURL()}/api/genres?platform=${platform}&lang=${lang}`)
+	const res = await fetch(`${baseURL}/api/genres?platform=${platform}&lang=${lang}`)
 
 	if(!res.ok){
 		throw new Error('Failed to fetch genres')
